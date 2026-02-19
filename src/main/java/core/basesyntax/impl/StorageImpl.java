1ef3c934ed3633f30f7e046c20bdeac13c6cdbbf
ReduxR <177857999+ReduxR@users.noreply.github.com>
2026-02-19 18:08:00 +0100

    logic for StorageImpl class created

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
-    }
+    private static final int MAX_ARRAY_SIZE = 11;
+
+    private final Object[] keys = new Object[MAX_ARRAY_SIZE];
+    private final Object[] values = new Object[MAX_ARRAY_SIZE];
+
+    private int size = 0;
 
     @Override
+    public void put(K key, V value) {
+        if (size >= MAX_ARRAY_SIZE) {
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        keys[size] = key;
+        values[size] = value;
+        size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
